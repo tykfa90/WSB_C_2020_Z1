48becf11410fd024ffee3a0eabbf537a30c45b2d
Konstanty Białek <tykfa90@gmail.com>
2020-04-29 12:30:19 +0200

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WSB_C_2020_Z1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
