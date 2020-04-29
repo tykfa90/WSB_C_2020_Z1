9fce8879880d962993284729f6b96b12629677c3
Konstanty Białek <tykfa90@gmail.com>
2020-04-29 16:46:37 +0200

    Zadanie 3.1 - gotowe

--- Program.cs
+++ Program.cs
@@ -8,15 +8,28 @@ namespace WSB_C_2020_Z1
         static void Main(string[] args)
         {
             BigInteger wynik = 0;
-            Console.WriteLine("Witam w programie do mnożenia liczb. \n\nProszę podać pierwszą liczbę do mnożenia: ");
+            Console.WriteLine("Witam w kalkulatorze. " +
+                "\nProszę wybrać typ działania z listy: " +
+                "\n1 - Dodawanie" +
+                "\n2 - Odejmowanie");
+            string opcja = Console.ReadLine();
+
+            Console.WriteLine("\n\nProszę podać pierwszą liczbę do działania: ");
             int liczba1 = int.Parse(Console.ReadLine());
 
             Console.WriteLine("Proszę podać drugą liczbę: ");
             int liczba2 = int.Parse(Console.ReadLine());
 
-            wynik = liczba1 * liczba2;
+            if (opcja == "1")
+            {
+                wynik = liczba1 + liczba2;
+            }
+            if (opcja == "2")
+            {
+                wynik = liczba1 - liczba2;
+            }
 
-            Console.WriteLine("Wynik mnożenia to: " + wynik);
+            Console.WriteLine("Wynik działania to: " + wynik);
 
             Console.ReadLine();
         }
