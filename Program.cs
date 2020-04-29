cec776b9b9e18d1975d8e6219669e1b1d03ba429
Konstanty Białek <tykfa90@gmail.com>
2020-04-29 17:08:37 +0200

    Zadanie 3.3

--- Program.cs
+++ Program.cs
@@ -12,7 +12,8 @@ namespace WSB_C_2020_Z1
                 "\nProszę wybrać typ działania z listy: " +
                 "\n1 - Dodawanie" +
                 "\n2 - Odejmowanie" +
-                "\n3 - Mnożenie");
+                "\n3 - Mnożenie." +
+                "\n4 - Dzielenie.");
             string opcja = Console.ReadLine();
 
             Console.WriteLine("\n\nProszę podać pierwszą liczbę do działania: ");
@@ -34,6 +35,15 @@ namespace WSB_C_2020_Z1
                 case "3":
                     Console.WriteLine("Wybrano mnożenie.");
                     wynik = liczba1 * liczba2;
+                    break;
+                case "4":
+                    Console.WriteLine("Wybrano dzielenie.");
+                    while (liczba2 == 0)
+                    {
+                        Console.WriteLine("Dzielenie przez zero nie jest możliwe! Proszę wybrać inną liczbę: ");
+                        liczba2 = int.Parse(Console.ReadLine());
+                    }
+                    wynik = liczba1 / liczba2;
                     break;
                 default:
                     break;
